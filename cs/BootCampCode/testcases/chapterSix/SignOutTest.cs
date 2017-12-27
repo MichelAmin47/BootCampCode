c0c72b693c17420da1dce9ad88661925226f9a86
Michel Lalmohamed <michel.lalmohamed@polteq.com>
2017-12-27 21:51:54 +0100

    SignOut test added

--- cs/BootCampCode/testcases/chapterSix/SignOutTest.cs
+++ cs/BootCampCode/testcases/chapterSix/SignOutTest.cs
@@ -8,12 +8,47 @@ namespace BootCampCode.testcases.chapterSix
     [TestClass]
     public class SignOutTest
     {
+        IWebDriver driver;
+
         [TestMethod]
         public void SignOut()
         {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
+
             driver.Navigate().GoToUrl("https://techblog.polteq.com/testshop/index.php");
-            driver.Manage().Window.Maximize();
+
+            //driver.Manage().Window.Maximize();
+
+            // Click on the login link
+            driver.FindElement(By.ClassName("login")).Click();
+
+            // Enter username
+            driver.FindElement(By.Id("email")).SendKeys("bootcamper@feelthepain.com");
+
+            // Enter password
+            driver.FindElement(By.Id("passwd")).SendKeys("1qazxsw2");
+
+            // Click the Log in button
+            driver.FindElement(By.Id("SubmitLogin")).Click();
+
+            // Verify if the logout link is displayed
+            Assert.IsTrue(driver.FindElement(By.CssSelector("a.logout")).Displayed, "Logout link should be displayed");
+
+
+            // Verify if the MY ACCOUNT text is show
+            String validationString = driver.FindElement(By.CssSelector("h1.page-heading")).Text;
+            Assert.AreEqual("MY ACCOUNT", validationString, "My account element was not found");
+
+            //Click the logout link
+            driver.FindElement(By.ClassName("logout")).Click();
+
+            Assert.AreEqual("Sign in", driver.FindElement(By.CssSelector(".login")).Text, "Check if the Sign in element is present");
+
+            //additional check
+            String className = driver.FindElement(By.CssSelector(".header_user_info:nth-child(2)>a")).GetAttribute("class");
+            Assert.AreEqual("login", className, "Check if the Sign in element is present");
+
+            driver.Quit();
         }
     }
 }
